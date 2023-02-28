c025153addf484a22bdc035eec638517cb035be3
Ivan Gordillo <ivanjavier.09.22@gmail.com>
2023-02-28 15:37:53 -0600

    Pequeño avance

--- strings&operaciones.py
+++ strings&operaciones.py
@@ -5,11 +5,11 @@ Dada una palabra, crea un string formado por los caracteres primero, medio y úl
 
 palabra = input("Ingrese una palabra: ")
 
-length_palabra = len(palabra)
+lengthWord = len(palabra)
 primero = palabra[0]
 ultimo  = palabra[len(palabra) - 1]
 
-if length_palabra % 2 == 0:
+if lengthWord % 2 == 0:
     medio = palabra[(lenght_palabra - 1) // 2] + palabra[(lenght_palabra + 1) // 2]
 else:
     medio = palabra[lenght_palabra // 2] 
@@ -25,12 +25,12 @@ Dada una palabra, crea un string hecha de los tres caracteres de enmedio.
 
 palabra = input('Ingrese una palabra: ')
 
-length_palabra = len(palabra)
+lengthWord = len(palabra)
 
-if length_palabra % 2 != 0:    
-    medio_1 = palabra[length_palabra // 2 - 1]
-    medio_2 = palabra[length_palabra // 2]
-    medio_3 = palabra[length_palabra // 2 + 1]
+if lengthWord % 2 != 0:    
+    medio_1 = palabra[lengthWord // 2 - 1]
+    medio_2 = palabra[lengthWord // 2]
+    medio_3 = palabra[lengthWord // 2 + 1]
     resultado = medio_1 + medio_2 + medio_3
 else:
     resultado = 'La palabra ingresada no es valida!'
@@ -42,16 +42,30 @@ print(resultado)
 Ejercicio 3.
 
 Agrega una nueva cadena en medio de un string dado.
-"""
+
 palabra = input("Ingrese una palabra: ")
 nuevaPalabra = input("Ingrese una palabra a agregar: ")
 
-length_palabra = len(palabra)
+lengthWord = len(palabra)
 
-if length_palabra % 2 != 0:
-    resultado = palabra[:(length_palabra - 1) // 2 ] + nuevaPalabra + palabra[(length_palabra + 1) // 2:]
+if lengthWord % 2 != 0:
+    resultado = palabra[:(lengthWord - 1) // 2 ] + nuevaPalabra + palabra[(lengthWord + 1) // 2:]
 else:
-    resultado = palabra[:length_palabra  // 2 ] + nuevaPalabra + palabra[length_palabra // 2:]
-
+    resultado = palabra[:lengthWord  // 2 ] + nuevaPalabra + palabra[lengthWord // 2:]
 
 print(resultado)
+"""
+
+"""
+Ejercicio 4.
+
+Dados tres strings de entrada, crea un nuevo string compuesto por el primero, medio y último carácter respectivamente de la primera cadena, de la segunda y de la tercera. 
+
+"""
+string_1 = input('Ingrese el primer string: ')
+string_2 = input('Ingrese el segundo string: ')
+string_3 = input('Ingrese el tercer string: ')
+
+if len(string_1) % 2 == 0:
+    
+mainString = 
